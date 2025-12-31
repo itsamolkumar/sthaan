13f62437c15bf2d52ec307cec92cb02b08ea4e38
itsamolkumar <itsamolkumar@gmail.com>
2025-12-31 20:46:02 +0530

    added links

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
   const backendUrl = import.meta.env.VITE_API_URL;
 //Axios interface to use it redux reducer............
 const api = axios.create({
-  baseURL: `http://localhost:8080/api`, // apne backend ka baseURL
+  baseURL: `${backendUrl}/api`, // apne backend ka baseURL
   withCredentials: true, // ✅ cookies bhejne ke liye
 });
 
