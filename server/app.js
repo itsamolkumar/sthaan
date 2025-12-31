3e4969fbf88943eb3d7b7c24c9e9185f5f437f22
itsamolkumar <itsamolkumar@gmail.com>
2025-12-31 21:41:02 +0530

    correcting some errors

--- server/app.js
+++ server/app.js
@@ -24,10 +24,18 @@ const allowedOrigins = [
   "https://sthaan-r1se4nlmx-amol-kumars-projects.vercel.app"
 ];
 app.use(cors({
-  origin: allowedOrigins,
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
   credentials: true
 }));
-app.options("*", cors());
+
+
+
 
 app.use(cookieParser());
 
